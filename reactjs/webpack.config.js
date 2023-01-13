b4894b8ecdeb86b78ee9266c8339961a8680837d
Sui Gn <natsynapses@gmail.com>
2023-01-13 13:44:24 -0600

    clients
    clients sides.

---
+++ reactjs/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  devServer: {
+    hot: true,
+    port: 3000,
+    proxy: {
+      '/': 'http://localhost:8080'
+    }
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
+};
