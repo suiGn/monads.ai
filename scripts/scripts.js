3f3650949a82d338569e629b7d3082e342eb69ae
Sui Gn <natsynapses@gmail.com>
2023-06-17 19:05:08 -0600

    2.0.3
    2.0.3

--- scripts/scripts.js
+++
@@ -1,8 +0,0 @@
-//show alert saying "Hello World"
-//alert("Hello World");
-
- //get IP address of web visitor
- $.getJSON("https://api.ipify.org?format=jsonp&callback=?",
- function(json) {
- console.log(json.ip);
-          });
